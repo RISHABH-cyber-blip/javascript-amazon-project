acdf42551004126589956cc0266316e41942c8f7
Rishabh <rm1994269@gmail.com>
2025-01-30 18:03:35 +0530

    add added button on page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ productsHTML+=`
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -41,7 +41,7 @@ productsHTML+=`
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -56,9 +56,12 @@ productsHTML+=`
 });
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
+const addedMessageTimeouts = {};
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click',() => {
+
      const productId=button.dataset.
      productId;
      let matchingItem;
@@ -84,5 +87,28 @@ document.querySelectorAll('.js-add-to-cart')
       cartQuantity+=item.quantity;
     });
     document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+    const addedMessage=document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add(`added-to-cart-visible`);
+
+
+   setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+    const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+
+      // Save the timeoutId for this product
+      // so we can stop it later if we need to.
+      addedMessageTimeouts[productId] = timeoutId;
+
+
   });
 });
