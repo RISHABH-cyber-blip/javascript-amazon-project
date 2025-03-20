659c6c39050d5bb55a7b55b093a4217781efa72d
Rishabh <rm1994269@gmail.com>
2025-03-20 15:47:27 +0530

    load products beforeAll hook

--- data/products.js
+++ data/products.js
@@ -72,7 +72,7 @@ object1.method();*/
 
 export let products=[]
 
-export function loadProducts(fun){
+export function loadProducts(fun=()=>{}){
   const xhr= new XMLHttpRequest();
   xhr.addEventListener('load',()=>{
     products = JSON.parse(xhr.response).map((productDetails)=>{
@@ -81,7 +81,6 @@ export function loadProducts(fun){
       }
      return new product(productDetails);
     });
-    console.log(products);
 
     fun();
   });
